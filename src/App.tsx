d049d4b88014eb891212249325ddd11b34e1c6e6
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-10-01 11:57:35 +0300

    initialized shaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber';
+import moonFragment from './moonFragment.glsl?raw';
+import moonVertex from './moonVertex.glsl?raw';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Canvas>
+				<scene>
+					<mesh>
+						<sphereGeometry args={[1, 32, 16]} attach="geometry" />
+						<shaderMaterial vertexShader={moonVertex} fragmentShader={moonFragment} attach="material" />
+					</mesh>
+				</scene>
+			</Canvas>
+		</div>
+	);
 }
 
 export default App;
